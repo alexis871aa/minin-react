b949369dafa4ad623121c479dab0002d2be8ba17
Aleksey Sokolov <alexis871aa@yandex.ru>
2024-02-06 10:03:22 +0300

    add setting eslint prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  )
-}
-
-export default App
+export const App = () => {
+	return (
+		<div>
+			<h1>Hello React!</h1>
+		</div>
+	);
+};
